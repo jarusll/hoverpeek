d9c160f9381ccab3b00b9ac877dfe712edc5c670
Suraj Yadav <ysuraj877@gmail.com>
2023-11-02 15:28:06 +0000

    Remove fetch call

--- background.js
+++ background.js
@@ -8,19 +8,6 @@ function connected(port) {
     console.log('recieved in background', { type, payload })
     switch (type) {
       case 'fetch':
-        fetch(payload)
-          .then((response) => {
-            return response.text()
-          })
-          .then((html) => {
-            var parser = new DOMParser();
-            var doc = parser.parseFromString(html, "text/html");
-            console.log('parsed : ', doc);
-            portFromCS.postMessage({ type: 'fetch', payload: JSON.parse(JSON.stringify(doc)) })
-          })
-          .catch((err) => {
-            console.log('Failed to fetch page: ', err);
-          });
         break
       default:
         () => { }
